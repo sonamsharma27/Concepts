ec5d0fdfeb9657a44ea1d64115abda7dc5061ec5
Sonam <akshu199bhardwaj@ggmail.com>
2025-09-22 21:33:49 +0530

    Added mac notes

---
+++ DSA/CP template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vs vector<string>
+#define ff first
+#define ss second
+
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	 
+	}
+	return 0;
+}
